e73edd2b57a00ebe76322772307a565aef9faebf
arindam341 <145852240+arindam341@users.noreply.github.com>
2025-12-25 19:43:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Full Stack Developer",
+    "Problem Solver",
+    "Entry-Level Web Developer"
+  ],
+  typeSpeed: 50,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
